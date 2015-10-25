4ac979b79acd2e193a4c5402b3de5db30e2514b8
Dom Batten <dominic.batten@googlemail.com>
2015-10-25 12:56:39 +0000

    first test failing

---
+++ test/toToListController.spec.js
@@ -0,0 +1,13 @@
+describe('ToDoListController', function() {
+  beforeEach(module('ToDoList'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ToDoListController');
+  }));
+
+  it('initialises with an empty list', function() {
+    expect(ctrl.listItems.length).toEqual(0);
+  });
+});
